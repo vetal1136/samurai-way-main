3659c2878afe60e809c786d1dc575000403190f8
vetal1136 <komrad188@yandex.ru>
2024-04-07 14:16:54 +0300

    1 sprint 2 week 1-13 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
 
 const App = () => {
     return (
-        <div>
+        <div className="app-wrapper">
             <Header/>
-            <Technologies/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQslCxnqZM0U1JGSL_vb-UkPqBSpXM_iDP46nYkKvUyFOEiK5U&s"/>
+        </header>
+    );
+};
+
+export default Header;
